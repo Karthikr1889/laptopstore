40a4303836ee0d491e00eb7d3b890437fac635a9
laptopstore <Karthikr1889@icloud.com>
2025-10-28 01:07:04 +0000

    Test update

---
+++ src/main/java/com/example/laptopstore/LaptopStoreApplicarion.java
@@ -0,0 +1,15 @@
+package com.example.laptopstore;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaptopStoreApplicarion {
+	public static void main(String[] args) {
+		
+        SpringApplication.run(LaptopStoreApplicarion.class, args);
+        System.out.println("Application started successfully");
+    }
+
+}
